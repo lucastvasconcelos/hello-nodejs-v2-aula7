9f699323adeac99b93761b41312f8086ff14e8b7
lucastvasconcelos <lucastavaresvasconcelos@gmail.com>
2016-12-02 13:10:16 -0300

    corrigindo duplicação no repositorio

--- public/servicos.js
+++
@@ -1,7 +0,0 @@
-angular.module("anguhello").service("eventoservice", function ($http){
-
-  this.buscaeventos = () => $http.get("eventos");
-
-  this.salvaevento = (ev) => $http.post("evento",ev);
-
-});
